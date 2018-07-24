515aa6fccd3cbd1d6613c0ccdcc193f42018fafc
cxy842723494 <cxy842723494@gmail.com>
2018-07-24 17:01:36 +0200

    binary image2

--- app/src/main/rs/binary.rs
+++ app/src/main/rs/binary.rs
@@ -11,14 +11,19 @@ uchar4 __attribute__((kernel)) root(uchar4 in,  uint32_t x, uint32_t y) {
   	float Y = 0.2989f * f4.r + 0.5870f * f4.g + 0.1140f * f4.b; // here y muss a Lower case letter, otherwise show error
 
     // check if it is more than the threshold, if it is, set to white, else make it black
-  	Y = Y > threshold ? 1.0f : 0.0f;
+  	//Y = Y > threshold ? 1.0f : 0.0f;
 
-  	//if (Y > threthold){
-  	 //   Y = 1.0f;
-  	//}
-  	//else {
-  	//    Y = 0.0f;
-  	//}
+  	if(Y > threshold)
+  	{
+  	    Y = 1.0f;
+  	    //return rsPackColorTo8888(1,1,1,f4.a);
+  	}
+  	else
+  	{
+        Y = 0.0f;
+        //return rsPackColorTo8888(0, 0, 0,f4.a);
+  	}
+
 
   	// set the pixel values to the computed value
   	f4.r = f4.g = f4.b = Y;
